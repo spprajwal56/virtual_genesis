babcfd2f8a0e437fb284f3f997f2948926f01fb2
spprajwal56 <59721178+spprajwal56@users.noreply.github.com>
2020-01-14 09:06:22 +0530

    Update fib_seeries.c

--- fib_seeries.c
+++ fib_seeries.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 void main()
 {
-    int i, n, t1, t2, nextTerm;
+    int i, n, t1=0, t2=1, nextTerm;
     printf("Enter the number of terms: ");
     scanf("%d", &n);
     printf("Fibonacci Series: ");
